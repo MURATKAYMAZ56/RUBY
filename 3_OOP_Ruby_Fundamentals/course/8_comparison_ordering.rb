290d7dfc256e2a788c8d733822726a274b341850
Murat Kaymaz <muratkaymaz@Murats-MacBook-Pro.local>
2021-01-24 20:51:29 +0100

    dkd

--- 3_OOP_Ruby_Fundamentals/course/8_comparison_ordering.rb
+++ 3_OOP_Ruby_Fundamentals/course/8_comparison_ordering.rb
@@ -1,4 +1,25 @@
+=begin
+    default imlementation
+    obj1==obj2 -> 0
+    otherwisise -> nil
+=end
 
-# use <=> to compare 2 objects
-# ob1==ob2 ->1
-#ob1
+=begin 
+    Needed implementation
+    use <=> to compare 2 objects
+    ob1==ob2 ->0
+    ob1 < obj2 -> -1
+    obj1 > obj2 -> 1
+
+=end
+
+class Collection
+    def <=>(other)
+        name<=> other.name
+    end
+end
+
+fiction=Collection.new("Fiction")
+fiction=Collection.new("Science")
+p fiction <=> science
+
