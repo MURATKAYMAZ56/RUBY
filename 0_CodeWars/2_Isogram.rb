ea04566bb3cf55c05e4ef3a9019eaccebf9ae859
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2021-01-27 01:52:15 +0100

    codewars

--- 0_CodeWars/2_Isogram.rb
+++ 0_CodeWars/2_Isogram.rb
@@ -1,10 +1,14 @@
 =begin
     An isogram is a word that has no repeating letters, 
     consecutive or non-consecutive. Implement a function that
-     determines whether a string that contains only letters is an isogram. 
-     Assume the empty string is an isogram. Ignore letter case.
+    determines whether a string that contains only letters is an isogram. 
+    Assume the empty string is an isogram. Ignore letter case.
+
+        is_isogram("Dermatoglyphics" ) == true
+        is_isogram("aba" ) == false
+        is_isogram("moOse" ) == false # -- ignore letter case
 =end
 
 def is_isogram(string)
-  string.downcase.chars.uniq.size == string.chars.size
+  string.downcase.chars.uniq == string.downcase.chars
 end
